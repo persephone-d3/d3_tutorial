72507f838cb1bcaefe559459241d419dc4fe1840
Frances Harlow <frances.harlow@gmail.com>
2016-01-19 11:46:58 -0500

    deletes geocoder

---
+++ bar-chart-test/main.js
@@ -0,0 +1,28 @@
+
+// define our data set
+var data = [4, 8, 15, 16, 23, 42];
+
+// d3.select(".chart")
+//   .selectAll("div")
+//   .data(data)
+//   .enter().append("div")
+//   .style("width", function(d) { 
+//     return d * 10 + "px"; 
+//   })
+//   .text(function(d) {
+//     return d;
+// });
+
+// select the chart div
+var chart = d3.select(".chart");
+// declare the elements we want to exist (aka initiate the data join)
+var bar = chart.selectAll("div");
+
+// join the data
+var barUpdate = bar.data(data);
+// returns empty so we instantiate the missing elements?
+var barEnter = barUpdate.enter().append("div");
+// set width
+barEnter.style("width", function(d) { return d * 10 + "px"; });
+// set text content
+barEnter.text(function(d) { return d; });
--- d3app/app.js
+++ d3app/app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app	= express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var geocoder = require('geocoder');
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
